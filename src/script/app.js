9dd6fc576561a887113e7f839f8edca78e1bf6f6
MrGuang <522125842@qq.com>
2016-01-28 23:23:38 +0800

    刚才没提交成功，在提交 一次

---
+++ src/script/app.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: GuangGe
+ * @Date:   2016-01-27 23:12:05
+ * @Last Modified by:   GuangGe
+ * @Last Modified time: 2016-01-28 17:07:27
+ */
+
+'use strict';
+console.log('nihao a ');
---
+++ src/script/demo.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: GuangGe
+ * @Date:   2016-01-28 17:07:38
+ * @Last Modified by:   GuangGe
+ * @Last Modified time: 2016-01-28 17:08:50
+ */
+
+'use strict';
+(function() {
+
+})();
+! function() {
+  console.log('wobuzhidaoa a a a ');
+}();
